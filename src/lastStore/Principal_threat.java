6abc9ba0eab8944885a82e02a18c7f89a7c7f5aa
velazquez638 <33837642+velazquez638@users.noreply.github.com>
2018-12-28 09:46:16 +0100

    Update Principal_threat.java

--- src/lastStore/Principal_threat.java
+++ src/lastStore/Principal_threat.java
@@ -9,7 +9,7 @@ public class Principal_threat {
 
 		Costumer cliente1 = new Costumer("Cliente 1", new int[] { 2, 2, 1, 5, 2, 3 });
 		Costumer cliente2 = new Costumer("Cliente 2", new int[] { 1, 3, 5, 1, 1 });
-		Costumer cliente3 = new Costumer("luis", new int[] { 1, 2, 5, 1, 1, 2, 4});
+		Costumer cliente3 = new Costumer("Cliente 3", new int[] { 1, 2, 5, 1, 1, 2, 4});
 
 
 		long initialTime = System.currentTimeMillis();
